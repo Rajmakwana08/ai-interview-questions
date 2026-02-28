c531431f92ecaf48c5a2e0cfa6d7075404fa357c
Rajmakwana08 <rajrajmakwana08@gmail.com>
2026-02-28 22:19:11 +0530

    practical no 6

--- src/App.tsx
+++ src/App.tsx
@@ -1875,10 +1875,106 @@ No Solution
       `
     },
     {
-      id: 1,
-      question: "1. ",
+      id: 6.6,
+      question: "6. Write a prolog program for the family tree.",
       answer: "",
-      codeExample: ``
+      codeExample: `
+/* SIMPLE FAMILY CHATBOT */
+
+% -------- FACTS --------
+male(john).
+male(paul).
+male(david).
+
+female(mary).
+female(lisa).
+
+parent(john, paul).
+parent(john, mary).
+parent(lisa, paul).
+parent(lisa, mary).
+parent(paul, david).
+
+% -------- RULES --------
+father(X, Y) :- parent(X, Y), male(X).
+mother(X, Y) :- parent(X, Y), female(X).
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+
+% -------- START --------
+:- initialization(main).
+
+main :-
+    write('FAMILY CHATBOT STARTED'), nl,
+    write('Type a query like father(john,paul).'), nl,
+    write('Type stop. to exit.'), nl,
+    chat.
+
+chat :-
+    read(Q),
+    ( Q = stop ->
+        write('Goodbye!'), nl
+    ;
+        answer(Q),
+        chat
+    ).
+
+% -------- RESPONSES --------
+answer(father(X,Y)) :-
+    ( father(X,Y) ->
+        write('Yes, correct.'), nl
+    ;
+        write('No, wrong.'), nl
+    ).
+
+answer(mother(X,Y)) :-
+    ( mother(X,Y) ->
+        write('Yes, correct.'), nl
+    ;
+        write('No, wrong.'), nl
+    ).
+
+answer(sibling(X,Y)) :-
+    ( sibling(X,Y) ->
+        write('Yes, they are siblings.'), nl
+    ;
+        write('No, they are not siblings.'), nl
+    ).
+
+answer(grandparent(X,Y)) :-
+    ( grandparent(X,Y) ->
+        write('Yes, correct.'), nl
+    ;
+        write('No, wrong.'), nl
+    ).
+
+answer(_) :-
+    write('I do not understand.'), nl.
+
+
+
+
+They must be typed in console like this:
+
+father(john,paul).
+mother(lisa,mary).
+sibling(paul,mary).
+grandparent(john,david).
+stop.
+
+
+OUTPUT:
+
+FAMILY CHATBOT STARTED
+Type a query like father(john,paul).
+Type stop. to exit.
+Yes, correct.
+Yes, correct.
+Yes, correct.
+Yes, correct.
+GoodBye!
+      
+      `
     },
     {
       id: 1,
